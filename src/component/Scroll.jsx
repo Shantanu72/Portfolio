da99fcc0f01a585e15d3f1cb9c98774816c1a15e
Ashish <harshitjoshi2002@gmail.com>
2025-07-02 12:21:27 +0530

    push about

--- src/component/Scroll.jsx
+++ src/component/Scroll.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import Experience from './inner-component/Experience';
 import Projects from './inner-component/Projects';
 import { Video } from './inner-component/Video';
+import About from './inner-component/About';
 
 function Scroll(){
     return(
       <div className='scroll-section'>
-   
+      <About/>
       <Projects/>
       <Experience/>
-     <Video/>
+      <Video/>
       </div>
     );
 }
---
+++ src/component/inner-component/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+    return ( 
+        <section className="about-section" id="about">
+            <div className="only-about">
+            <h4>ABOUT</h4>
+            </div>
+        <p>In 2012, my family and I had an unforgettable trip to the Taj Mahal. We got a new camera to capture the special moments, but unfortunately, I accidentally deleted all the photos, leading to a scolding and a profound sense of guilt. This incident sparked my passion for computer science, pushing me to explore digital data recovery to retrieve those lost memories. Since then, I've been captivated by the limitless possibilities and innovations that the world of computer science has to offer.</p>
+        <p>My main focus these days is strengthening my computer science skills, specializing in web technology. I'm always up for thrilling coding adventures, aiming to gain expertise and expand my knowledge. In my free time I enjoy writing blogs on computer science and emerging web technologies.</p>
+        <p>When I’m not at the computer, I’m usually playing football, hanging out with my friends and family, or running around Dehradun & Mussoorie searching for a new places to explore.</p>
+        </section>
+     );
+}
+
+export default About;

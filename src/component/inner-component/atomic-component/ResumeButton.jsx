fb936d9ae50922f4f03327a40831bf1e61387ec4
Ashish <harshitjoshi2002@gmail.com>
2025-07-11 23:57:36 +0530

    final

--- src/component/inner-component/atomic-component/ResumeButton.jsx
+++ src/component/inner-component/atomic-component/ResumeButton.jsx
@@ -5,7 +5,7 @@ export function ResumeButton() {
     return (
         <a
             href={data.downloadLink}
-            download={"shantanu_pradhan.pdf"}
+            // download={"shantanu_pradhan.pdf"}
             className="inline-flex items-center justify-center gap-2 px-6 py-3 bg-gradient-to-br from-[#8CEDFF] via-[#1ED5FA] to-[#06C1FF] text-[#ffff] font-semibold text-base rounded-xl shadow-md hover:shadow-lg hover:scale-[1.02] active:scale-[0.98] transition-all duration-300 ease-in-out no-underline"
         >
             <MdDownload className="text-white" size={20} />
